7fa7358e0f42c85d3fd2c41cdc97e074fbce3ffb
vasilisalda0528 <vasilisalda0528@outlook.com>
2024-06-26 23:35:45 +0400

    splash primary

--- src/App.js
+++ src/App.js
@@ -1,20 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
-import { BrowserRouter, Routes as Router, Route } from "react-router-dom";
+import { BrowserRouter, Routes as Router, Route, Navigate } from "react-router-dom";
 
 import Earn from "pages/Earn/Earn";
+import Splash from "pages/Splash/splash";
 
 import Homepage from "./pages/Home";
-import Wallet from './pages/Wallet/wallet';
+import Wallet from "./pages/Wallet/wallet";
 
 const App = () => {
+  const [gohome, setGohome] = useState(0);
+  let retrieve1,retrieve2;
+  if (!gohome) retrieve1 = setTimeout(() => setGohome(1), 3000);
+  useEffect(() => {
+    if (gohome == 1){ 
+      clearTimeout(retrieve1)
+      retrieve2 = setTimeout(() => setGohome(2), 500);
+    }
+    if (gohome == 2) {
+      setGohome(3);
+      clearTimeout(retrieve2);
+    }
+  }, [gohome]);
+  const handle = () => {
+    if (gohome == 0) {
+      console.log(gohome);
+      return <Navigate to="/splash" />;
+    } else if (gohome == 1) {
+      return <Navigate to="/" />;
+    }
+  };
+  //
   return (
     <BrowserRouter>
       <Router>
-        <Route path="/"   element={<Homepage/> } key='1'/>
-        <Route path="/earn"  element={<Earn/> } />
-        <Route path="/wallet"  element={<Wallet/> } />
+        <Route path="/splash" element={<Splash />} />
+        <Route path="/" element={<Homepage />} key="1" />
+        <Route path="/earn" element={<Earn />} />
+        <Route path="/wallet" element={<Wallet />} />
       </Router>
+      {handle()}
     </BrowserRouter>
   );
 };
---
+++ src/pages/Splash/splash.js
@@ -0,0 +1,10 @@
+import './styles.css'
+
+const Splash = () =>{
+    return <div className="splash">
+        <div className='splash-logo'></div>
+    </div>
+}
+
+
+export default Splash;
